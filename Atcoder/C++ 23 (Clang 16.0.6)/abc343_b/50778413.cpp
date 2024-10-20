952e6c7987381028f71358c6b1a194b30a4b3b3a
Firas Frigui <94382065+snaakefoot@users.noreply.github.com>
2024-10-20 22:30:09 +0100

    Committing files

---
+++ Atcoder/C++ 23 (Clang 16.0.6)/abc343_b/50778413.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+//#pragma comment(linker, "/STACK:20000000")
+using namespace std;
+#define FAST ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
+const ll mod=1e9 +7;
+const ll mxint=1e6+1;
+
+
+void solve() {
+	int n,m,l,r;cin>>n>>m;
+	if(n+m!=0)
+		cout<<n+m-1<<endl;
+	else
+		cout<<1<<endl;
+}
+
+
+int main(){
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+
+	FAST;
+	int t=1;
+	while(t--){
+		solve();
+	}
+			
+    return 0;
+}
