bbe529b88bde0cb24aa16a2b8bfc4095d4f91129
snaakefoot <firas.frigui@gmail.com>
2024-10-27 21:29:41 +0100

    Codeforces

---
+++ Codeforces/Codeforces Global Round 27/A.cpp
@@ -0,0 +1,28 @@
+  #include<bits/stdc++.h>
+ 
+using namespace std;
+using ll =  long long;
+const ll mod=1e9+7;
+const int MAXN=2*1e5+1;
+ 
+void solve() {
+	ll n,m,r,c,ans=0;cin>>n>>m>>r>>c;
+	cout<<(2ll*m-1)*(n-r)+m-c<<endl;
+	
+	
+	
+	
+}
+ 
+int main(){
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+ 
+	int t;cin>>t;
+ 
+	while(t--){
+		solve();
+	}
+	
+    return 0;
+}
